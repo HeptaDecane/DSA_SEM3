eb3097b59a3abcdeceee0c5467d276b10a4abc30
NearLawiet <sooraj1999vs@gmail.com>
2019-09-09 23:53:11 +0530

    added .gitignore file

--- a6-2.cpp
+++ a6-2.cpp
@@ -26,9 +26,6 @@ Node::Node(float coe,int exp){
 	exponent=exp;
 	link=NULL;
 }
-
-
-
 class List{
 	Node *head;
 public:
@@ -39,10 +36,24 @@ public:
 	void insert(float,int);
 	void display();
 	void getPolynomial();
+	void arrange();
 	float evaluate(float);
+	void menu();
+	void free();
 	List add(List);
 };
-/*Under Construction*/
+void List::free(){
+	head->link=head;
+}
+void List::menu(){
+	cout<<"\n-----------------------MENU----------------------------";
+	cout<<"\n\tPress 1: Enter Polynomial";
+	cout<<"\n\tPress 2: Display Polynomial";
+	cout<<"\n\tPress 3: Evaluate Polynomial at x";
+	cout<<"\n\tPress 4: Add Another Polynomial";
+	cout<<"\n\tPress 9: Show Menu";
+	cout<<"\n-------------------------------------------------------\n";
+}
 List List::add(List exp2){
 	List e;
 	Node *p,*q;
@@ -60,7 +71,27 @@ List List::add(List exp2){
 		else if((p->exponent)<(q->exponent)){
 			coe=q->coefficient;
 			exp=q->exponent;
+			e.insert(coe,exp);
+			q=q->link;
 		}
+		else{
+			coe=p->coefficient+q->coefficient;
+			exp=p->exponent;
+			if(coe)
+				e.insert(coe,exp);
+			p=p->link;
+			q=q->link;
+		}
+	}
+	for(;p!=head;p=p->link){
+		coe=p->coefficient;
+		exp=p->exponent;
+		e.insert(coe,exp);
+	}
+	for(;q!=exp2.head;q=q->link){
+		coe=q->coefficient;
+		exp=q->exponent;
+		e.insert(coe,exp);
 	}
 	return e;
 }
@@ -83,12 +114,13 @@ void List::getPolynomial(){
 	float coe;
 	cout<<"\nEnter Number of Terms: ";
 	cin>>n;
-	cout<<"Enter Terms in order [Coefficient< >Exponent]\n";
+	cout<<"Enter Terms in as: Coefficient <space> Exponent\n";
 	for(int i=0;i<n;i++){
 		cout<<"Term "<<i+1<<": ";
 		cin>>coe>>exp;
 		insert(coe,exp);
 	}
+	arrange();
 	return;
 }
 void List::insert(float coe,int exp){
@@ -115,14 +147,75 @@ void List::display(){
 		return;
 	}
 	while(t!=head){
-		cout<<"\n"<<t->coefficient<<" "<<t->exponent;
+		if(t->exponent){
+			if(t->coefficient>0){
+				cout<<" + "<<t->coefficient<<"x^"<<t->exponent;
+			}
+			else{
+				cout<<" - "<<-t->coefficient<<"x^"<<t->exponent;
+			}
+		}
+		else{
+			if(t->coefficient>0){
+				cout<<" + "<<t->coefficient;
+			}
+			else{
+				cout<<" - "<<-t->coefficient;
+			}
+		}
 		t=t->link;
 	}
 	return;
 }
+void List::arrange(){
+	int exp;
+	float coe;
+	Node *p,*q;
+	for(p=head->link;p!=head;p=p->link){
+		for(q=p->link;q!=head;q=q->link){
+			if(p->exponent<q->exponent){
+				exp=p->exponent;
+				coe=p->coefficient;
+				
+				p->exponent=q->exponent;
+				p->coefficient=q->coefficient;
+				
+				q->exponent=exp;
+				q->coefficient=coe;
+			}
+		}
+	}
+}
 int main() {
-	List a,b;
+	int ch=1;
+	List exp1,exp2,sum;
 	float x;
-	a.getPolynomial();
-	a.display();
+	exp1.menu();
+	while(ch){
+		switch(ch){
+			case 1:	cout<<"Enter Experssion:";
+					exp1.free();
+					exp1.getPolynomial();
+					break;
+			case 2: cout<<"\nP(x) =";
+					exp1.display();
+					break;
+			case 3: cout<<"\nEnter Evaluation Value: ";cin>>x;
+					cout<<"p("<<x<<") = "<<exp1.evaluate(x);
+					break;
+			case 4: cout<<"Enter 2nd Experssion:";
+					exp2.getPolynomial();
+					sum=exp1.add(exp2);
+					cout<<"\n(";exp1.display();cout<<" ) + (";exp2.display();cout<<" )\n";
+					cout<<"\t\t=";sum.display();
+					break;
+			case 9: exp1.menu();
+					break;
+			default: cout<<"\n Enter Valid Choice!";
+		}
+		cout<<"\n-------------------------------------------------------";
+		cout<<"\nEnter Choice [0-exit]: ";cin>>ch;
+	}
+	cout<<"\nEND...";
+	return 0;
 }
