9271e3caa9e0b204d3e0ef4d27cbecbce3507d52
NearLawiet <sooraj1999vs@gmail.com>
2019-09-22 16:55:19 +0530

    Modification on a8

--- a8.cpp
+++ a8.cpp
@@ -16,8 +16,8 @@ public:
 	Stack(){
 		top=-1;
 	}
-	
-	
+
+
 	void push(T a){
 		top++;
 		if(top<MAX)
@@ -25,41 +25,40 @@ public:
 		else{
 			cout<<"Stack is FULL";
 			top--;
-		}	
+		}
 	}
 	T peek(){
 		if(top==-1)
 			return '#';
 		return arr[top];
 	}
-	
-	
+
+
 	void pop(){
 		if(top==-1){
-			cout<<"*Undefined*";
-			cout<<"\n*Operator(s) Mismatch";
+			cout<<"Stack is EMPTY";
 			return;
 		}
 		top--;
 	}
-	
-	
+
+
 	bool isEmpty(){
 		return top==-1;
 	}
-	
-	
+
+
 	bool isFull(){
 		return top==MAX;
 	}
-	
-	
+
+
 	void display(){
 		for(int i=top;i>=0;i--)
 			cout<<arr[i]<<" ";
 	}
-	
-	
+
+
 	~Stack(){
 		top=-1;
 	}
@@ -76,7 +75,6 @@ public:
 	}
 	void read();
 	void display();
-	bool isValid();
 	void append(char);
 	int getLength();
 	int precedence(char);
@@ -101,7 +99,7 @@ void Expression::display(){
 }
 void Expression::append(char c){
 	int i;
-	for(i=0;str[i]!=0;i++);
+	for(i=0;str[i]!='\0';i++);
 	str[i]=c;
 	str[i+1]='\0';
 }
@@ -117,44 +115,6 @@ int Expression::precedence(char op){
 	else if(op=='*'||op=='/')
 		return 2;
 	else if(op=='+'||op=='-')
-		return 1;
-	else
-		return 0;
-}
-bool Expression::isValid(){
-	Stack<char> s;
-	if(str[0]=='}'||str[0]==')'||str[0]==']'){
-		return 0;
-	}
-	for(int i=0;str[i]!='\0';i++){
-		if(str[i]=='{')
-			s.push(str[i]);
-		else if(str[i]=='[')
-			s.push(str[i]);
-		else if(str[i]=='(')
-			s.push(str[i]);
-		if(!s.isEmpty()){
-			if(str[i]==')'&&s.peek()=='(')
-				s.pop();
-			else if(str[i]==')'&&s.peek()!='(')
-				return 0;
-			if(str[i]=='}'&&s.peek()=='{')
-				s.pop();
-			else if(str[i]=='}'&&s.peek()!='{')
-				return 0;
-			if(str[i]==']'&&s.peek()=='[')
-				s.pop();
-			else if(str[i]==']'&&s.peek()!='[')
-				return 0;
-		}
-		else if(s.isEmpty()){
-			if(str[i]=='}'||str[i]==')'||str[i]==']'){
-				return 0;
-				break;
-			}
-		}
-	}
-	if(s.isEmpty())
 		return 1;
 	else
 		return 0;
@@ -210,15 +170,15 @@ float Expression::evaluate(){
 	for(int i=0;str[i]!='\0';i++){
 		symbol=str[i];
 		if(isalpha(symbol)){
-			cout<<"*Undefined*";
-			cout<<"\n*Ambiguity in Variables!";
-			return 0;
+			cout<<"Enter Value of "<<symbol<<": ";
+			cin>>a;
+			s.push(a);
 		}
 		else if(isdigit(symbol)){
 			a=symbol-'0';
 			s.push(a);
 		}
-		else if(isOperator(symbol)){
+		else if(isOperator(symbol)&&!s.isEmpty()){
 			b=s.peek();
 			s.pop();
 			a=s.peek();
@@ -234,24 +194,27 @@ float Expression::evaluate(){
 							break;
 			}
 		}
+		else{
+			cout<<"*Undefined*";
+			cout<<"\n*Operator(s) MisMatch";
+			return 0;
+		}
 	}
-	s.display();
-	return 0;
+	cout<<str<<" = ";
+	return s.peek();
 }
 int main(){
 	Expression infix,postfix;
 	int rep=1;
-	float val;
 	while(rep){
 		infix.read();
 		postfix=infix.infixToPostfix();
 		if(postfix.getLength()){
 			cout<<"Postfix Expression is: ";
 			postfix.display();
-			cout<<"\nEvaluation: ";
+			cout<<"\n\nEvaluation: ";
 			cout<<"\n";
-			infix.display();cout<<"\b = ";postfix.evaluate();
-			
+			cout<<postfix.evaluate();
 		}
 		else
 			cout<<"\nInvalid Infix Expression!";
