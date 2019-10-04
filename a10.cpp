6a569e3e19ca3ea52ce78563e013c3be0641a4bd
NearLawiet <sooraj1999vs@gmail.com>
2019-10-05 00:56:46 +0530

    Sat Oct 5, 00:56

--- a10.cpp
+++ a10.cpp
@@ -9,109 +9,130 @@
 using namespace std;
 #define MAX 10
 class Deque{
-	int arr[MAX];
+	int array[MAX];
 	int front;
 	int rear;
-	int size;
 public:
 	Deque();
 	void menu();
-	void pushFront(int);
-	void pushBack(int);
-	void popFront();
-	void popBack();
+	void enqueueFront(int);
+	void enqueueRear(int);
+	void dequeueFront();
+	void dequeueRear();
 	void display();
 	void display0();
 	bool isEmpty();
 	bool isFull();
 	int peekFront();
-	int peekBack();
+	int peekRear();
 };
 
 Deque::Deque(){
-	front=0;
-	rear=-1;
-	size=0;
-	for(int i=0;i<MAX;i++)
-		arr[i]=0;
+	front=-1;
+	rear=0;
+	for(int i=0;i<10;i++)
+		array[i]=0;
 }
 
 bool Deque::isEmpty(){
-	return(size<=0);
+	return(front==-1);
 }
 
 bool Deque::isFull(){
-	return (size==MAX);
+	return((front==0&&rear==MAX-1)||front==rear+1);
 }
 
-void Deque::pushFront(int key){
+void Deque::enqueueFront(int key){
 	if(isFull()){
 		cout<<"\nError: Deque Full!";
 		return;
 	}
-	size++;
-	arr[front]=key;
-	front=(front+1)%MAX;
-	cout<<key<<" Added at Front";
+	if(front==-1)
+		front=rear=0;
+	else if(front==0)
+		front=MAX-1;
+	else 
+		front=front-1;
+	array[front]=key;
+	cout<<"\n"<<key<<" Inserted at Front!";
 }
 
-void Deque::pushBack(int key){
+void Deque::enqueueRear(int key){
 	if(isFull()){
 		cout<<"\nError: Deque Full!";
 		return;
 	}
-	size++;
-	arr[rear+MAX]=key;
-	rear=(rear-1)%MAX;
-	cout<<key<<" Added at Rear";
+	if(front==-1)
+		front=rear=0;
+	else if(rear==MAX-1)
+		rear=0;
+	else
+		rear=rear+1;
+	array[rear]=key;
+	cout<<"\n"<<key<<" Inserted at Rear!";
 }
 
-void Deque::popFront(){
+void Deque::dequeueFront(){
 	if(isEmpty()){
 		cout<<"\nError: Deque Empty!";
 		return;
 	}
-	size--;
-	cout<<"\nFront Popped!";
-	front=(front-1)%MAX;
+	array[front]=0;
+	if(front==rear)
+		front=rear=-1;
+	else if(front==MAX-1)
+		front=0;
+	else
+		front=front+1;
+	cout<<"\nDeleted Front Element!";
 }
 
-void Deque::popBack(){
+void Deque::dequeueRear(){
 	if(isEmpty()){
 		cout<<"\nError: Deque Empty!";
 		return;
 	}
-	size--;
-	cout<<"\nRear Popped!";
-	rear=(rear+1)%MAX;
+	array[rear]=0;
+	if(front==rear)
+		front=rear=-1;
+	else if(rear==0)
+		rear=MAX-1;
+	else
+		rear=rear-1;
+	cout<<"\nDeleted Rear Element!";	
 }
-
 int Deque::peekFront(){
-	int index=(front-1)%MAX;
-	return(arr[index]);
+	if(!isEmpty())
+		return(array[front]);
 }
 
-int Deque::peekBack(){
-	int index=(rear+1)%MAX+MAX;
-	return(arr[index]);
+int Deque::peekRear(){
+	if(!isEmpty())
+		return(array[rear]);
 }
 
 void Deque::display(){
+	int i=front;
 	cout<<"\nDeque: ";
-	for(int i=front-1;i>rear;i--){
-		if(i>=0)
-			cout<<arr[i%MAX]<<" ";
-		else
-			cout<<arr[i%MAX+MAX]<<" ";
-	}
-	cout<<"\nNo. of Elements in Deque = "<<size;
-	return;
+	if(front<=rear)
+		for(int i=front;i<=rear;i++)
+			cout<<array[i]<<" ";
+	else
+		while(true){
+			cout<<array[i]<<" ";
+			if(i==rear)
+				break;
+			else if(i==MAX-1)
+				i=0;
+			else
+				i++;
+		}
 }
 
 void Deque::display0(){
 	cout<<"\n";
 	for(int i=0;i<MAX;i++)
-		cout<<arr[i]<<" ";
+		cout<<array[i]<<" ";
 	cout<<"\nfront = "<<front;
 	cout<<"\nrear = "<<rear;
 }
@@ -119,11 +140,12 @@ void Deque::display0(){
 void Deque::menu(){
 	cout<<"\n-----------------DEQUE OPERATIONS-------------------";
 	cout<<"\nPress 1: Push Element at Front";
-	cout<<"\nPress 2: Push Element at Back";
+	cout<<"\nPress 2: Push Element at Rear";
 	cout<<"\nPress 3: Pop Element at Front";
-	cout<<"\nPress 4: Pop Element at Back";
-	cout<<"\nPress 5: Display Deque";
-//	cout<<"\nPress 8: Developer Display;";
+	cout<<"\nPress 4: Pop Element at Rear";
+	cout<<"\nPress 5: Peek Front Element";
+	cout<<"\nPress 6: Peek Rear Element";
+	cout<<"\nPress 7: Display Deque";	
 	cout<<"\nPress 9: Display Operations";
 }
 int main(){
@@ -136,19 +158,23 @@ int main(){
 		cin>>choice;
 		switch(choice){
 			case 1: cout<<"\nEnter Element: ";cin>>key;
-					container.pushFront(key);
+					container.enqueueFront(key);
 					break;
 			case 2: cout<<"\nEnter Element: ";cin>>key;
-					container.pushBack(key);
+					container.enqueueRear(key);
 					break;
-			case 3: container.popFront();
+			case 3: container.dequeueFront();
 					break;
-			case 4: container.popBack();
+			case 4: container.dequeueRear();
 					break;
-			case 5: container.display();
+			case 5: cout<<"\nFront Element = "<<container.peekFront();
 					break;
-//			case 8: container.display0();
-//					break;
+			case 6: cout<<"\nRear Element = "<<container.peekRear();
+					break;
+			case 7: container.display();
+					break;
+			case 8: container.display0();
+					break;
 			case 9: container.menu();
 					break;
 			case 0: cout<<"\n----------------------END---------------------------\n";
